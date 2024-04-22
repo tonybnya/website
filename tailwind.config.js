5627857d7b480ff7adeea265f4a4e69143cbe64d
tonybnya <nya.tony2010@gmail.com>
2024-04-22 17:55:36 +0100

    style: add different fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        lato: ["lato", "sans-serif"],
+        latobold: ["lato-bold", "sans-serif"],
+        montserrat: ["montserrat", "sans-serif"],
+        montserratbold: ["montserrat-bold", "sans-serif"],
+        opensans: ["open-sans", "sans-serif"],
+        opensansbold: ["open-sans-bold", "sans-serif"],
+        sourcesans3: ["source-sans-3", "sans-serif"],
+        sourcesans3bold: ["source-sans-3-bold", "sans-serif"],
+        righteous: ["righteous", "sans-serif"],
+      }
+    },
   },
   plugins: [],
 };
