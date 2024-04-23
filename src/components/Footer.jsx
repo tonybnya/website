aa563ddba671a6e277ea31e124c1c65d2edbfe77
tonybnya <nya.tony2010@gmail.com>
2024-04-23 20:41:54 +0100

    fix: responsive for the footer at breakpoint min-width: 640px

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,15 +4,15 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <footer className="shadow bottom-0 mb-0 bg-penn-blue font-opensans">
-      <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between xs:flex xs:flex-col xs:justify-center">
-        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+      <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between max-sm:text-center">
+        <span className="text-sm sm:text-center text-gray-400">
           © 2024{" "}
           <Link to="/" className="hover:underline">
             TAF
           </Link>
           . Tous Droits Réservés.
         </span>
-        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-400 sm:mt-0 max-sm:block max-sm:flex max-sm:justify-center">
           <li>
             <Link to="/about" className="hover:underline me-4 md:me-6">
               A Propos
