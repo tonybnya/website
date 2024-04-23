7ace878c32d6fff0ded0b3afd2ae4ff9f356f9d1
tonybnya <nya.tony2010@gmail.com>
2024-04-23 21:34:34 +0100

    fix: whole responsive design of the HeroHome component

--- src/components/HeroHome.jsx
+++ src/components/HeroHome.jsx
@@ -17,13 +17,13 @@ const HeroHome = () => {
       <section className="bg-dark-goldenrod flex items-center">
         <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 max-sm:flex max-sm:flex-col max-sm:items-center">
           <div className="mr-auto place-self-center lg:col-span-7">
-            <h1 className="max-w-2xl mb-4 text-5xl font-extrabold font-montserratbold tracking-tight leading-none text-penn-blue">
+            <h1 className="max-w-2xl mb-4 text-5xl font-extrabold font-montserratbold tracking-tight leading-none text-penn-blue max-[1439px]:text-center max-sm:text-center max-sm:text-2xl">
               TAF Equipment & Logistics
             </h1>
-            <h3 className="font-montserrat mb-6 text-penn-blue">
+            <h3 className="font-montserrat mb-6 text-penn-blue max-sm:text-sm max-sm:text-center max-[1439px]:text-center">
               Logistics Simplified. Equipment Empowered.
             </h3>
-            <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl text-white font-opensans text-justify">
+            <p className="max-w-2xl mb-6 font-light lg:mb-8 md:text-lg lg:text-xl text-white font-opensans text-justify">
               De la gestion des stocks à la livraison internationale, les
               entreprises et partenaires sollicitant des services du secteur de
               l&apos;quipement et de la logistique comptent sur{" "}
@@ -35,7 +35,7 @@ const HeroHome = () => {
               délicates, nous offrons une solution complète. La logistique
               simplifiée et l&apos;équipement au service de la performance.
             </p>
-            <div className="max-sm:text-center">
+            <div className="max-sm:text-center max-[1439px]:text-center">
               <Link
                 to="/services"
                 className="bg-penn-blue hover:bg-indigo-dye inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300"
@@ -56,14 +56,14 @@ const HeroHome = () => {
               </Link>
               <Link
                 to="/contact"
-                className="hover:bg-indigo-dye inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 text-white"
+                className="hover:bg-indigo-dye inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center border border-gray-300 rounded-lg focus:ring-4 focus:ring-gray-100 text-white"
               >
                 Nous Contacter
               </Link>
             </div>
           </div>
 
-          <div className="relative w-full lg:mt-0 lg:col-span-5 lg:flex max-sm:mt-6">
+          <div className="relative w-full lg:col-span-5 lg:flex max-sm:mt-6 max-w-lg mx-auto lg:max-w-none lg:mx-0 mt-6 lg:mt-0">
             <div className="max-w-lg">
               <HomeCarousel slides={slides} />
             </div>
