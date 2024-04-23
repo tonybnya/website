d7595ebb3c38c5963d26c9b080b719da853c2cb3
tonybnya <nya.tony2010@gmail.com>
2024-04-23 18:53:10 +0100

    feat: create the carousel of the hero section of the home page

---
+++ src/components/HomeCarousel.jsx
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from "react";
+import { ChevronLeft, ChevronRight } from "react-feather";
+
+const HomeCarousel = ({
+  autoSlide = false,
+  autoSlideInterval = 3000,
+  slides,
+}) => {
+  const [curr, setCurr] = useState(0);
+
+  const prev = () =>
+    setCurr((curr) => (curr === 0 ? slides.length - 1 : curr - 1));
+  const next = () =>
+    setCurr((curr) => (curr === slides.length - 1 ? 0 : curr + 1));
+
+  useEffect(() => {
+    if (!autoSlide) return;
+    const slideInterval = setInterval(next, autoSlideInterval);
+    return () => clearInterval(slideInterval);
+  }, []);
+
+  return (
+    <div className="overflow-hidden relative">
+      <div
+        className="flex transition-transform ease-out duration-500"
+        style={{ transform: `translateX(-${curr * 100}%)` }}
+      >
+        {slides.map((img, index) => (
+          <img
+            key={index}
+            src={img}
+            className="rounded-3xl"
+            alt="slide-image"
+          />
+        ))}
+      </div>
+      <div className="absolute inset-0 flex items-center justify-between p-4">
+        <button
+          onClick={prev}
+          className="p-1 rounded-full shadow bg-white/80 text-gray-800 hover:bg-white"
+        >
+          <ChevronLeft size={40} />
+        </button>
+        <button
+          onClick={next}
+          className="p-1 rounded-full shadow bg-white/80 text-gray-800 hover:bg-white"
+        >
+          <ChevronRight size={40} />
+        </button>
+      </div>
+
+      <div className="absolute bottom-4 right-0 left-0">
+        <div className="flex items-center justify-center gap-2">
+          {slides.map((_, index) => (
+            <div
+              key={index}
+              className={`
+              transition-all w-3 h-3 bg-white rounded-full
+              ${curr === index ? "p-2" : "bg-opacity-50"}
+            `}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomeCarousel;
