f28706290ef24743cb82a73bdf0550093c17b1b7
tonybnya <nya.tony2010@gmail.com>
2024-04-26 15:47:50 +0100

    fix: style and rearrange the Error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import error from "/assets/img/error.png";
-import "../styles/error.css";
 
 const ErrorPage = () => {
   return (
-    <div>
-      <img src={error} alt="error 404 image" />
-      <h2>Error - The page is not found!</h2>
-      <Link to="/" className="link">
-        <span>Go back to the homepage</span>
+    <div className="flex flex-col items-center justify-center my-[10vh]">
+      <img src={error} alt="error 404 image" className="w-1/4" />
+      <h2 className="font-opensans mt-4 text-red-500">
+        Erreur - Cette page n&apos;existe pas !
+      </h2>
+      <Link
+        to="/"
+        className="mt-12 bg-penn-blue text-white font-montserratbold px-6 py-4 rounded-md hover:bg-dark-goldenrod"
+      >
+        <span>Retournez sur la page d&apos;accueil</span>
       </Link>
     </div>
-  )
-}
+  );
+};
 
 export default ErrorPage;
+
