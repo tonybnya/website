3c78bec79d4670cfe301fe3f1e7e7bb54982e65a
tonybnya <nya.tony2010@gmail.com>
2024-04-27 08:54:01 +0100

    fix: routing features in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -7,60 +7,97 @@ import Contact from "./pages/Contact";
 import ErrorPage from "./pages/ErrorPage";
 import Footer from "./components/Footer";
 
-const Layout = () => {
+const Layout = ({ children }) => {
   return (
     <div>
       <Navbar />
-      <Outlet />
+      {children}
       <Footer />
     </div>
   );
 };
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/about",
-        element: <About />,
-      },
-      {
-        path: "/services",
-        element: <Services />,
-      },
-      {
-        path: "/contact",
-        element: <Contact />,
-      },
-    ],
-  },
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "contact",
-    element: <Contact />,
-  },
-  {
-    path: "*",
-    element: <ErrorPage />,
-  },
-]);
-
 const App = () => {
   return (
-    <>
-      <RouterProvider router={router} />
-    </>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </Layout>
+    </Router>
   );
 };
 
 export default App;
 
+// import { BrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+// import Navbar from "./components/Navbar";
+// import Home from "./pages/Home";
+// import About from "./pages/About";
+// import Services from "./pages/Services";
+// import Contact from "./pages/Contact";
+// import ErrorPage from "./pages/ErrorPage";
+// import Footer from "./components/Footer";
+//
+// const Layout = () => {
+//   return (
+//     <div>
+//       <Navbar />
+//       <Outlet />
+//       <Footer />
+//     </div>
+//   );
+// };
+//
+// const router = BrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: "/",
+//         element: <Home />,
+//       },
+//       {
+//         path: "/about",
+//         element: <About />,
+//       },
+//       {
+//         path: "/services",
+//         element: <Services />,
+//       },
+//       {
+//         path: "/contact",
+//         element: <Contact />,
+//       },
+//     ],
+//   },
+//   {
+//     path: "/",
+//     element: <Home />,
+//   },
+//   {
+//     path: "contact",
+//     element: <Contact />,
+//   },
+//   {
+//     path: "*",
+//     element: <ErrorPage />,
+//   },
+// ]);
+//
+// const App = () => {
+//   return (
+//     <>
+//       <RouterProvider router={router} />
+//     </>
+//   );
+// };
+//
+// export default App;
