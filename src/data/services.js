8925090b802a94f7747bf6c99f77f3f05b698cde
tonybnya <nya.tony2010@gmail.com>
2024-05-06 13:43:05 +0100

    update services

--- src/data/services.js
+++ src/data/services.js
@@ -1,5 +1,11 @@
 const services = [
   {
+    icon: "/assets/icon/8.svg",
+    service: "Vente d'engins",
+    description:
+      "Nous comprenons l'importance de disposer d'équipements fiables pour vos opérations logistiques. Notre gamme de services comprend la vente d'équipements, d'engins et de camions de logistique de haute qualité, conçus pour répondre aux besoins variés de nos clients.",
+  },
+  {
     icon: "/assets/icon/5.svg",
     service: "Maintenance d'engins",
     description:
@@ -12,6 +18,30 @@ const services = [
       "Besoin d'équipements fiables pour vos projets ? Notre service de location d'engins propose une gamme variée d'équipements de qualité, adaptés à vos exigences. De la construction au transport, nous avons ce qu'il vous faut.",
   },
   {
+    icon: "/assets/icon/9.svg",
+    service: "Conseil",
+    description:
+      "Le Conseil est au cœur de notre approche. Nous comprenons que chaque entreprise est unique, avec ses propres défis et opportunités. C'est pourquoi nous offrons des services de conseil personnalisés pour vous aider à maximiser votre efficacité opérationnelle, à réduire vos coûts et à atteindre vos objectifs commerciaux.",
+  },
+  {
+    icon: "/assets/icon/7.svg",
+    service: "Prestation de Services",
+    description:
+      "Notre engagement envers l'excellence se reflète dans notre gamme de services complets. Que ce soit la consultation, la formation ou la gestion de projet, nous sommes là pour toujours vous servir au mieux.",
+  },
+  {
+    icon: "/assets/icon/6.svg",
+    service: "Commerce Général",
+    description:
+      "De la fourniture de matières premières à la distribution de produits finis, notre commerce général répond à tous vos besoins. Avec une sélection diversifiée et des partenariats solides, nous sommes votre guichet unique pour toutes vos demandes commerciales.",
+  },
+  {
+    icon: "/assets/icon/4.svg",
+    service: "Import-Export",
+    description:
+      "Facilitant les échanges internationaux, nous simplifions le processus d'importation et d'exportation pour votre entreprise. Grâce à notre expertise et nos partenariats mondiaux, vos transactions sont fluides et rentables.",
+  },
+  {
     icon: "/assets/icon/1.svg",
     service: "Logistique",
     description:
@@ -22,24 +52,6 @@ const services = [
     service: "Transport",
     description:
       "Notre réseau de transport efficace garantit une livraison rapide et sûre de vos marchandises, où que vous soyez. Avec une attention méticuleuse aux détails et une flotte moderne, nous assurons une satisfaction client à chaque étape.",
-  },
-  {
-    icon: "/assets/icon/4.svg",
-    service: "Import-Export",
-    description:
-      "Facilitant les échanges internationaux, nous simplifions le processus d'importation et d'exportation pour votre entreprise. Grâce à notre expertise et nos partenariats mondiaux, vos transactions sont fluides et rentables.",
-  },
-  // {
-  //   icon: "/assets/icon/6.svg",
-  //   service: "Commerce Général",
-  //   description:
-  //     "De la fourniture de matières premières à la distribution de produits finis, notre commerce général répond à tous vos besoins. Avec une sélection diversifiée et des partenariats solides, nous sommes votre guichet unique pour toutes vos demandes commerciales.",
-  // },
-  {
-    icon: "/assets/icon/7.svg",
-    service: "Prestation de Services",
-    description:
-      "Notre engagement envers l'excellence se reflète dans notre gamme de services complets. Que ce soit la consultation, la formation ou la gestion de projet, nous sommes là pour toujours vous servir au mieux.",
   },
 ];
 
