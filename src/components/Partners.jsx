74d2f666818f6f2c34f28ce36708f34cba5047ec
tonybnya <nya.tony2010@gmail.com>
2024-05-07 10:29:08 +0100

    feat: start design Partners component

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -3,8 +3,9 @@ import React from "react";
 const Partners = () => {
   return (
     <>
-      <h1>Ils Nous Ont Choisi</h1>
-      <h1>Ils Nous Ont Choisi</h1>
+      <h1 className="text-center drop-shadow-2xl font-righteous mb-12 text-4xl max-sm:text-2xl text-penn-blue underline">
+        Ils Nous Ont Choisi...
+      </h1>
     </>
   );
 };
